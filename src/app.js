06315099c8b99e3a8fd61dd9617e30fb96c7f8e5
Laura Spieker <ljspiek@gmail.com>
2020-03-24 20:55:23 -0500

    updated CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
@@ -13,7 +14,10 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    }))
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
