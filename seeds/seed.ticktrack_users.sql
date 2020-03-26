52064b8679e493db2e4c6ac9ce88669c63bbf765
Laura Spieker <ljspiek@gmail.com>
2020-03-25 19:56:33 -0500

    migrations, seeds, basic server

---
+++ seeds/seed.ticktrack_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO ticktrack_users (email, password)
+VALUES
+('test@testing.com', 'password1'),
+('user@user.com', 'password2'),
+('lastone@thatsit.com', 'password3');
--- src/app.js
+++ src/app.js
@@ -5,12 +5,27 @@ const cors = require('cors')
 const {CLIENT_ORIGIN} = require('./config')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const winston = require('winston')
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 app.use(morgan(morganOption))
 app.use(helmet())
@@ -19,6 +34,19 @@ app.use(
         origin: CLIENT_ORIGIN
     }))
 
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
+    
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
