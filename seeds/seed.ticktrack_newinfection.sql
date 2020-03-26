65d6406d6f11030a579fe62040fe19941d56d333
Laura Spieker <ljspiek@gmail.com>
2020-03-25 20:44:35 -0500

    migrations and seeds for basic tables

---
+++ seeds/seed.ticktrack_newinfection.sql
@@ -0,0 +1,6 @@
+INSERT INTO ticktrack_newinfections (indicator)
+VALUES
+('New Tick Bite'),
+('New spotted rash over large area'),
+('New "EM" rash (discrete circle)'),
+('New linear, red streaks');
