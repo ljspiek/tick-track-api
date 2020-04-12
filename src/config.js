5b45073db4134f01ead98c82eb5cf4cbc94472b3
Laura Spieker <ljspiek@gmail.com>
2020-04-12 17:44:49 -0500

    trying to deploy to heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || '://postgres@localhost/tick-track',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/tick-track',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
       "http://localhost:3000/api"
